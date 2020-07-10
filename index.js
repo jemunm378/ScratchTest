9ca0e95d29e41025b9bcf93970def315ec91bb29
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-07-10 09:45:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-fetch("https://api.scratch.mit.edu/proxy/featured").then(res => res.json()).then(console.log)
+fetch("https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/proxy/featured").then(res => res.json()).then(console.log)
